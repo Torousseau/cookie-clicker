2bfff7ec7f9fc18ce4d8473e3d8a7e150778a70a
Mobelli <151550846+Torousseau@users.noreply.github.com>
2025-05-15 13:15:15 +0200

    Update save

--- src/pages/CookieClicker.jsx
+++ src/pages/CookieClicker.jsx
@@ -10,7 +10,7 @@ import StatsPage from '../components/StatsPage.jsx';
 import AchievementPage from '../components/Achievements.jsx';
 import AchievementNotification from '../components/AchievementNotification.jsx';
 import achievementsData from '../data/achievements.json';
-import useSaveLoadCookies from '../hooks/useSaveLoadCookies';
+// import useSaveLoadCookies from '../hooks/useSaveLoadCookies';
 
 function CookieClicker() {
     const {
